a1fccf77cfb590af149b641df1e896ec06a270a8
linmeng <1812323571@qq.com>
2022-04-01 16:08:12 +0800

    git 简单修改

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import ElementUI, {Message} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
 createApp(App).mount('#app')
